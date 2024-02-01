404d2af8cf800e181aaf652d5ed29b77588a9db9
wglee0511 <ranazang@naver.com>
2024-02-01 10:49:02 +0900

    Initial Commit

---
+++ SwiftUIContentModeViewer/SwiftUIContentModeViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContentModeViewerApp.swift
+//  SwiftUIContentModeViewer
+//
+//  Created by racoon on 2024/02/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContentModeViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
